c1086fe0a2b2971201589373ca99f6b564d61858
Nitin <jthawkn@jci.com>
2018-12-15 14:21:03 +0530

    Custome validation Blur event and Change event for derop doown/select  list
    Custome validation Blur event and Change event for derop doown/select  list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,13 @@ export class AppComponent {
   title = 'app';
   topics = ['Angular', 'VUE', 'React', 'Bootsrtap'];
 
-  userModel = new User('', 'nit@gmail.com', 9999988888, '', 'morning', true);
+  userModel = new User('', 'nit@gmail.com', 9999988888, 'default', 'morning', true);
+  topicHasError = true;
+  validateTopic(value) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
 }
