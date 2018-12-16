39f2e798534c9b22ac71c4b982510f86bcd3ee5d
Nitin <jthawkn@jci.com>
2018-12-16 19:15:17 +0530

    form builder Alternate way to create form group and form control
    Alternate way to create form group and form control

--- src/app/react-from/react-from.component.ts
+++ src/app/react-from/react-from.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
  import {ReactiveFormsModule} from '@angular/forms'; // add in app module
 
 @Component({
@@ -9,27 +10,36 @@ import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 })
 export class ReactFromComponent implements OnInit {
 
-  constructor() { }
+  registrationForm = this.fb.group({
+    userName: ['Nitinv'],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: ['fsb'],
+      state: [''],
+      postalCode: ['']
+    })
+  });
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
-
   // registrationForm = new FormGroup({
   //   userName: new FormControl('Vishwas'),
   //   password: new FormControl(''),
   //   confirmPassword: new FormControl(''),
   // });
 
-  registrationForm = new FormGroup({
-    userName: new FormControl('Vishwas'),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    address: new FormGroup({
-      city: new FormControl(''),
-      state: new FormControl(''),
-      postalCode: new FormControl('')
-    })
-  });
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Vishwas'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // });
 
 
   loadAPIData() {
