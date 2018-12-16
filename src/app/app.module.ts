ece65226b65e18d2630eb4939fc3bdf73bfdc92d
Nitin <jthawkn@jci.com>
2018-12-16 18:10:08 +0530

    Creating the Form Model
    Creating the Form Model
    FormGroup, FormControl
    Impoert in app module
    import {ReactiveFormsModule} from '@angular/forms'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http'
-
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule , routingComponents} from './/app-routing.module';
 //import { ReactFromComponent } from './react-from/react-from.component';
@@ -17,6 +17,7 @@ import { AppRoutingModule , routingComponents} from './/app-routing.module';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/react-from/react-from.component.ts
+++ src/app/react-from/react-from.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+ import {ReactiveFormsModule} from '@angular/forms'; // add in app module
 
 @Component({
   selector: 'app-react-from',
@@ -12,4 +14,21 @@ export class ReactFromComponent implements OnInit {
   ngOnInit() {
   }
 
+  registrationForm = new FormGroup({
+    userName: new FormControl('Vishwas'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
+
+ // registrationForm: FormGroup;
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Vishwas'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // });   
 }
