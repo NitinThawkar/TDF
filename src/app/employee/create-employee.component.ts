8c1ec2f43e5e20f4d6181e2a5a991918731ea307
Nitin <jthawkn@jci.com>
2018-12-22 10:34:30 +0530

    8-Angular formbuilder example
    8-Angular formbuilder example

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormGroupDirective } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -10,17 +10,29 @@ export class CreateEmployeeComponent implements OnInit {
 
   employeeForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.employeeForm = new FormGroup({
-      fullName: new FormControl(),
-      email: new FormControl(),
-      skills : new FormGroup({
-        skillName: new FormControl(),
-        experienceInYears: new FormControl(),
-        proficiency: new FormControl()
+
+    // this.employeeForm = new FormGroup({
+    //   fullName: new FormControl(),
+    //   email: new FormControl(),
+    //   skills : new FormGroup({
+    //     skillName: new FormControl(),
+    //     experienceInYears: new FormControl(),
+    //     proficiency: new FormControl()
+    //   })
+    // });
+
+    this.employeeForm = this.fb.group({
+      fullName: [],
+      email: [],
+      skills: this.fb.group({
+        skillName: [],
+        experienceInYears: [],
+        proficiency: []
       })
+
     });
   }
 
