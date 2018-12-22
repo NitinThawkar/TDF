878476a3f4a6db3c7cbdab1656dd2ed3a773562b
Nitin <jthawkn@jci.com>
2018-12-22 22:25:23 +0530

    15-Angular reactive form custom validator
    15-Angular reactive form custom validator

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -33,7 +33,8 @@ export class CreateEmployeeComponent implements OnInit {
       'maxlength': 'Full Name must be less than 10 characters.'
     },
     'email': {
-      'required': 'Email is required.'
+      'required': 'Email is required.',
+      'emailDomain': 'Email domian should be pragimtech.com'
     },
     'phone': {
       'required': 'phone is required.'
@@ -62,7 +63,7 @@ export class CreateEmployeeComponent implements OnInit {
     // });
     this.employeeForm = this.fb.group({
       fullName: ['nitin', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
-      email: ['nitin@gmail.com', Validators.required],
+      email: ['nitin@gmail.com', [Validators.required, emailDomain]],
       contactPreference: ['email'],
       phone: [],
       skills: this.fb.group({
@@ -229,4 +230,14 @@ onContactPrefernceChange(selectedValue: string) {
 }
 
 
+}
+
+function emailDomain(control: AbstractControl): { [key: string]: any } | null {
+  const email: string = control.value;
+  const domain = email.substring(email.lastIndexOf('@') + 1);
+  if (email === '' || domain.toLowerCase() === 'pragimtech.com') {
+    return null;
+  } else {
+    return { 'emailDomain': true };
+  }
 }
