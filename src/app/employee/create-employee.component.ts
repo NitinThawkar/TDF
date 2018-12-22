7dcb1956449563e3bd6d9f098ff3db3787a10285
Nitin <jthawkn@jci.com>
2018-12-22 19:36:02 +0530

    12-Move validation messages to the component class in reactive form
    12-Move validation messages to the component class in reactive form

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -14,6 +14,34 @@ export class CreateEmployeeComponent implements OnInit {
 
   nameLength = 0;
 
+  // This object will hold the messages to be displayed to the user
+  // Notice, each key in this object has the same name as the
+  // corresponding form control
+  formErrors = {
+    'fullName': '', 'email': '', 'skillName': '',
+    'experienceInYears': '', 'proficiency': ''
+  };
+  // This object contains all the validation messages for this form
+  validationMessages = {
+    'fullName': {
+      'required': 'Full Name is required.',
+      'minlength': 'Full Name must be greater than 2 characters.',
+      'maxlength': 'Full Name must be less than 10 characters.'
+    },
+    'email': {
+      'required': 'Email is required.'
+    },
+    'skillName': {
+      'required': 'Skill Name is required.',
+    },
+    'experienceInYears': {
+      'required': 'Experience is required.',
+    },
+    'proficiency': {
+      'required': 'Proficiency is required.',
+    },
+  };
+
   ngOnInit() {
 
     // this.employeeForm = new FormGroup({
@@ -27,11 +55,11 @@ export class CreateEmployeeComponent implements OnInit {
     // });
     this.employeeForm = this.fb.group({
       fullName: ['nitin', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
-      email: ['nitin@gmail.com'],
+      email: ['nitin@gmail.com' , Validators.required],
       skills: this.fb.group({
-        skillName: ['Ang'],
-        experienceInYears: [5],
-        proficiency: ['beginner']
+        skillName: ['Ang' , Validators.required],
+        experienceInYears: [5 , Validators.required],
+        proficiency: ['' , Validators.required]
       }),
     });
 
@@ -100,7 +128,44 @@ export class CreateEmployeeComponent implements OnInit {
 
    onLoadDataClick(): void {
     this.logKeyValuePairs(this.employeeForm);
+
+    //12-Move validation messages to the component class in reactive form
+    this.logValidationErrors(this.employeeForm);
+    console.log(this.formErrors);
+  
    }
+
+
+  logValidationErrors(group: FormGroup): void {
+    // Loop through each control key in the FormGroup
+    Object.keys(group.controls).forEach((key: string) => {
+      // Get the control. The control can be a nested form group
+      const abstractControl = group.get(key);
+      // If the control is nested form group, recursively call
+      // this same method
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+        // If the control is a FormControl
+      } else {
+        // Clear the existing validation errors
+        this.formErrors[key] = '';
+        if (abstractControl && !abstractControl.valid) {
+          // Get all the validation messages of the form control
+          // that has failed the validation
+          const messages = this.validationMessages[key];
+          // Find which validation has failed. For example required,
+          // minlength or maxlength. Store that error message in the
+          // formErrors object. The UI will bind to this object to
+          // display the validation errors
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {
+              this.formErrors[key] += messages[errorKey] + ' ';
+            }
+          }
+        }
+      }
+    });
+  }
 
 
    logKeyValuePairs(group: FormGroup): void {
