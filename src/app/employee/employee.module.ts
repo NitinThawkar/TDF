354d6d46a594953cfd07194425f6c8c541fdbb35
Nitin <jthawkn@jci.com>
2018-12-25 00:40:19 +0530

    34-Creating shared module in angular
    34-Creating shared module in angular

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';  // custome module  user define module
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+//import { CommonModule } from '@angular/common'; //moved to shared module
+//import { ReactiveFormsModule } from '@angular/forms'; //moved to shared module
 
 import {EmployeeRoutingModule} from './employee-routing.module';
 
 import { CreateEmployeeComponent } from './create-employee.component';
 import { ListEmployeeComponent } from './list-employee.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    EmployeeRoutingModule
+    //CommonModule, //moved to shared module
+   // ReactiveFormsModule, //moved to shared module
+    EmployeeRoutingModule,
+    SharedModule
   ],
   declarations: [CreateEmployeeComponent, ListEmployeeComponent]
 })
